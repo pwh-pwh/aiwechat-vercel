11fcc1c5a9bb3cd9e656fee9343e19ec5d800907
pwh-pwh <plmoknpwh@163.com>
2024-02-28 21:48:53 +0800

    add wx simple response

---
+++ api/wx.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"fmt"
+	"github.com/silenceper/wechat/v2"
+	"github.com/silenceper/wechat/v2/cache"
+	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
+	"github.com/silenceper/wechat/v2/officialaccount/message"
+	"net/http"
+	"os"
+)
+
+func Wx(rw http.ResponseWriter, req *http.Request) {
+	token := os.Getenv("TOKEN")
+	wc := wechat.NewWechat()
+	memory := cache.NewMemory()
+	cfg := &offConfig.Config{
+		AppID:     "",
+		AppSecret: "",
+		Token:     token,
+		Cache:     memory,
+	}
+	officialAccount := wc.GetOfficialAccount(cfg)
+
+	// 传入request和responseWriter
+	server := officialAccount.GetServer(req, rw)
+	//设置接收消息的处理方法
+	server.SetMessageHandler(func(msg *message.MixMessage) *message.Reply {
+		//回复消息：演示回复用户发送的消息
+		text := message.NewText(msg.OpenID + "" + msg.Content)
+		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+	})
+
+	//处理消息接收以及回复
+	err := server.Serve()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//发送回复的消息
+	server.Send()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module github.com/pwh-pwh/aiwechat-vercel
 
 go 1.21
 
-require github.com/sashabaranov/go-openai v1.20.2 // indirect
+require (
+	github.com/bradfitz/gomemcache v0.0.0-20220106215444-fb4bf637b56d // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/sashabaranov/go-openai v1.20.2 // indirect
+	github.com/silenceper/wechat/v2 v2.1.6 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/tidwall/gjson v1.14.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
