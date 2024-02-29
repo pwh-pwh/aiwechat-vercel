0202257d639d5f46888f1973a945918254d296a3
pwh-pwh <plmoknpwh@163.com>
2024-02-29 22:51:01 +0800

    add readme and timeout control

---
+++ README.md
@@ -0,0 +1,1 @@
+# aiwechat-vercel
--- chat/chat.go
+++ chat/chat.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pwh-pwh/aiwechat-vercel/config"
 	"github.com/sashabaranov/go-openai"
 	"os"
+	"time"
 )
 
 type BaseChat interface {
@@ -26,28 +27,48 @@ func (e *Echo) Chat(userID string, msg string) string {
 }
 
 type SimpleGptChat struct {
-	token string
-	url   string
+	token    string
+	url      string
+	cacheMsg map[string]string
 }
 
 func (s *SimpleGptChat) Chat(userID string, msg string) string {
+	if s.cacheMsg[userID] != "" {
+		r := s.cacheMsg[userID]
+		delete(s.cacheMsg, userID)
+		return r
+	}
 	cfg := openai.DefaultConfig(s.token)
 	cfg.BaseURL = s.url
 	client := openai.NewClientWithConfig(cfg)
-	resp, err := client.CreateChatCompletion(context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: msg,
+	resChan := make(chan string)
+	go func() {
+		resp, err := client.CreateChatCompletion(context.Background(),
+			openai.ChatCompletionRequest{
+				Model: openai.GPT3Dot5Turbo,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleUser,
+						Content: msg,
+					},
 				},
-			},
-		})
-	if err != nil {
-		return err.Error()
+			})
+		if err != nil {
+			resChan <- err.Error()
+			return
+		}
+		resChan <- resp.Choices[0].Message.Content
+	}()
+
+	select {
+	case res := <-resChan:
+		return res
+	case <-time.After(4 * time.Second):
+		go func() {
+			s.cacheMsg[userID] = <-resChan
+		}()
+		return "响应内容过长，重新发送任意回复获取答复"
 	}
-	return resp.Choices[0].Message.Content
 }
 
 func GetChatBot() BaseChat {
@@ -65,8 +86,9 @@ func GetChatBot() BaseChat {
 			url = "https://api.openai.com/v1/"
 		}
 		return &SimpleGptChat{
-			token: os.Getenv("GPT_TOKEN"),
-			url:   url,
+			token:    os.Getenv("GPT_TOKEN"),
+			url:      url,
+			cacheMsg: make(map[string]string),
 		}
 	case config.ECHO:
 		return &Echo{}
