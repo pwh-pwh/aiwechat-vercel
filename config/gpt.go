a83af9dfc9e0a39a510feb6673e8db198651b6ea
故人 <111191349+88lin@users.noreply.github.com>
2024-03-24 23:32:20 +0800

    Update gpt.go

--- config/gpt.go
+++ config/gpt.go
@@ -10,7 +10,7 @@ const (
 func GetGptWelcomeReply() (r string) {
 	r = os.Getenv(Gpt_Welcome_Reply_Key)
 	if r == "" {
-		r = "我是gpt，开始聊天吧！"
+		r = "gpt-3.5-turbo-16k-0613模型配置成功，我们开始聊天吧！"
 	}
 	return
 }
